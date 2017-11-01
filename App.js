521552bf68726928ff35eda8b9cf9aa09f085fd0
michaelsnook <michael.snook@gmail.com>
2017-11-01 19:07:35 +0530

    Add react-navigation and move default view into /screens folder

--- App.js
+++ App.js
@@ -1,63 +1,10 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
+import { StackNavigator } from 'react-navigation';
+import LoginScreen from './src/LoginScreen.js';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={[styles.container, styles.homeScreen, styles.minPadding, {flex: 1}]}>
-        <Text
-          style={[styles.title, styles.whiteText]}
-        >
-          Welcome to
-        </Text>
-        <Text
-          style={[styles.title, styles.whiteText, {fontWeight: 'bold'}]}
-        >
-          Sunlo
-        </Text>
-        <Button
-          style={{marginTop: 30, marginBottom: 20}}
-          buttonStyle={{backgroundColor: 'white', width: '100%'}}
-          textStyle={{color: '#025aa5'}}
-          title='Log in'
-          onPress={ () => console.log('logging in') }
-        />
-        <Button
-          buttonStyle={{width: '100%', backgroundColor: 'transparent'}}
-          title='Sign up'
-          onPress={ () => console.log('signing up') }
-          underlayColor='transparent'
-        />
-      </View>
-    );
-  }
-
-
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  minPadding: {
-    padding: 5,
-  },
-  homeScreen: {
-    backgroundColor: '#025aa5',
-    flex: 1,
-  },
-  whiteText: {
-    color: 'white',
-  },
-  title: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginLeft: 20,
-    marginRight: 20,
-  },
+export default App = StackNavigator({
+  Home: { screen: LoginScreen },
+  //Profile: { screen: ProfileScreen },
 });
