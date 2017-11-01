fb2833e40d15e179a229b153e479f9cb724f7fab
michaelsnook <michael.snook@gmail.com>
2017-11-02 03:03:46 +0530

    Load /screeens/LoginScreen.js properly

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
 import { StackNavigator } from 'react-navigation';
-import LoginScreen from './src/LoginScreen.js';
+import LoginScreen from './screens/LoginScreen.js';
 
 export default App = StackNavigator({
   Home: { screen: LoginScreen },
